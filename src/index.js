6ddbf8742e1e17915cb4cfd5b84f1a19c1715384
RicardoQC <ricardoqc@byzoop.com>
2024-04-14 07:40:42 -0500

    Se agrego http, se arreglo funcionalidad

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import http from 'http';
-import { getUsuarios, exportCSV, importCSV } from './controller';
+import { getUsers, exportToCSV, importFromCSV } from './controller.js';
 
 const server = http.createServer((req, res) => {
+  res.json = (data) => {
     res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(data));
+  };
 
-    if (req.url === '/api/users') {
-        getUsuarios(req, res);
-    } else if (req.url === '/api/users/export') {
-        exportCSV(req, res);
-    } else if (req.url === '/api/users/import') {
-        importCSV(req, res);
-    } else {
-        res.statusCode = 400;
-        res.end(JSON.stringify({ error: 'Ruta no encontrada'}));
-    }
+  res.status = (statusCode) => {
+    res.statusCode = statusCode;
+    return res;
+  };
+
+  if (req.url === '/api/users' && req.method === 'GET') {
+    getUsers(req, res);
+  } else if (req.url === '/api/users/export' && req.method === 'GET') {
+    exportToCSV(req, res);
+  } else if (req.url === '/api/users/import' && req.method === 'POST') {
+    importFromCSV(req, res);
+  } else {
+    res.status(404).json({ error: 'Endpoint no encontrado' });
+  }
 });
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port}`);
+  console.log(`Servidor corriendo en el puerto ${port}`);
 });
